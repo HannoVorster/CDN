898fd227f800b5ab7247393b4742129454a29dc4
Hanno Vorster <hanno.vorster@bigengroup.com>
2023-07-23 22:46:57 +0200

    Add Typical Day chart

--- charts.js
+++ charts.js
@@ -1,3 +1,4 @@
+// Historical Chart
 const historical = (
   height,
   values,
@@ -141,8 +142,199 @@ const historical = (
   return options;
 };
 
-const Charts = {
-  historical: historical,
+// Typical Day Chart
+const typicalDay = (height, data, yAxisText, tooltipYMetric) => {
+  const max = data.map((a) => a.max);
+  const perc90 = data.map((a) => a.percentile90);
+  const perc75 = data.map((a) => a.percentile75);
+  const avg = data.map((a) => a.avg);
+  const perc25 = data.map((a) => a.percentile25);
+  const perc10 = data.map((a) => a.percentile10);
+  const min = data.map((a) => a.min);
+  const times = data.map(
+    (a) => `${a.Time.split(":")[0]}:${a.Time.split(":")[1]}`
+  );
+
+  var options = {
+    series: [
+      {
+        name: "Max",
+        data: max,
+      },
+      {
+        name: "90th Percentile",
+        data: perc90,
+      },
+      {
+        name: "75th Percentile",
+        data: perc75,
+      },
+      {
+        name: "Average",
+        data: avg,
+      },
+      {
+        name: "25th Percentile",
+        data: perc25,
+      },
+      {
+        name: "10th Percentile",
+        data: perc10,
+      },
+      {
+        name: "Min",
+        data: min,
+      },
+    ],
+    dataLabels: {
+      enabled: false,
+    },
+    chart: {
+      type: "area",
+      height: height,
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      animations: {
+        enabled: false,
+      },
+      fontFamily: "inherit",
+    },
+    colors: [
+      "#ececec",
+      "#bbbbbb ",
+      "#747474",
+      "#000000",
+      "#747474",
+      "#bbbbbb ",
+      "#ececec",
+    ],
+    legend: {
+      showForSingleSeries: true,
+      position: "top",
+      labels: {
+        colors: "#565674",
+      },
+    },
+    stroke: {
+      curve: "smooth",
+      width: [0, 0, 0, 2, 0, 0, 0],
+      dashArray: [0, 0, 0, 0, 0, 0, 0],
+    },
+    fill: {
+      type: "solid",
+      opacity: 1,
+      colors: [
+        (value) => {
+          if (value.seriesIndex == 2 || value.seriesIndex == 3) {
+            return "#747474";
+          } else if (value.seriesIndex == 1 || value.seriesIndex == 4) {
+            return "#bbbbbb";
+          } else if (value.seriesIndex == 0 || value.seriesIndex == 5) {
+            return "#ececec";
+          } else {
+            return "#ffffff";
+          }
+        },
+      ],
+      gradient: "none",
+    },
+    xaxis: {
+      tickAmount: 24,
+      categories: times,
+      axisBorder: {
+        show: false,
+      },
+      title: {
+        text: "Time",
+        style: {
+          color: "#565674",
+          fontSize: "12px",
+        },
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        rotateAlways: true,
+        style: {
+          colors: "#565674",
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      forceNiceScale: true,
+      min: 0,
+      title: {
+        text: yAxisText,
+        style: {
+          color: "#565674",
+          fontSize: "12px",
+        },
+      },
+      labels: {
+        formatter: (val) => {
+          return Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 0,
+          }).format(val);
+        },
+        style: {
+          colors: "#565674",
+          fontSize: "12px",
+        },
+      },
+      tickAmount: 5,
+    },
+    grid: {
+      strokeDashArray: 4,
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+    },
+    tooltip: {
+      style: {
+        fontSize: "12px",
+      },
+      y: {
+        formatter: (val) => {
+          return (
+            Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(
+              val
+            ) + tooltipYMetric
+          );
+        },
+      },
+    },
+    noData: {
+      text: "No data to display...",
+      align: "center",
+      verticalAlign: "middle",
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+        color: "#000000",
+        fontSize: "16px",
+        fontFamily: "Helvetica",
+      },
+    },
+  };
+
+  return options;
 };
 
-//module.exports = Charts;
+// Add all functions in an object
+const Charts = {
+  historical: historical,
+  typicalDay: typicalDay,
+};
