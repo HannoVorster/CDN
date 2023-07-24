4a0ab9988a2a5a39193c99af7e56a66f7ee6cdc7
Hanno Vorster <hanno.vorster@bigengroup.com>
2023-07-24 20:14:32 +0200

    Add reservoir chart

--- charts.js
+++ charts.js
@@ -1,13 +1,5 @@
 // Historical Chart
-const historical = (
-  height,
-  values,
-  minDate,
-  maxDate,
-  yAxisText,
-  toolTipXFormat,
-  tooltipYMetric
-) => {
+const historical = (height, values, minDate, maxDate, yAxisText, toolTipXFormat, tooltipYMetric) => {
   var options = {
     series: values,
     chart: {
@@ -41,18 +33,7 @@ const historical = (
         },
       },
     },
-    colors: [
-      "#f78f1e",
-      "#3359FF",
-      "#7333FF",
-      "#30a6d3",
-      "#775DD0",
-      "#3F51B5",
-      "#4CAF50",
-      "#2B908F",
-      "#2E294E",
-      "#D7263D",
-    ],
+    colors: ["#f78f1e", "#3359FF", "#7333FF", "#30a6d3", "#775DD0", "#3F51B5", "#4CAF50", "#2B908F", "#2E294E", "#D7263D"],
     legend: {
       showForSingleSeries: true,
       position: "top",
@@ -129,13 +110,148 @@ const historical = (
           if (isNaN(val) || val == null) {
             return "No Data";
           }
-          return (
-            Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(
-              val
-            ) + tooltipYMetric
-          );
+          return Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(val) + tooltipYMetric;
         },
       },
+    },
+  };
+
+  return options;
+};
+
+// Historical Reservoir Chart
+const historicalReservoir = (height, values, minDate, maxDate, yAxisText, toolTipXFormat) => {
+  var options = {
+    series: values,
+    chart: {
+      type: "line",
+      height: height,
+      toolbar: {
+        show: true,
+        offsetY: 300,
+        tools: {
+          selection: false,
+          zoomin: false,
+          zoomout: false,
+          pan: false,
+        },
+      },
+      zoom: {
+        enabled: true,
+      },
+      animations: {
+        enabled: false,
+      },
+      fontFamily: "inherit",
+      events: {
+        beforeResetZoom: function (chartContext, opts) {
+          return {
+            xaxis: {
+              min: moment(minDate).startOf("h").valueOf(),
+              max: moment(maxDate).endOf("h").valueOf(),
+            },
+          };
+        },
+      },
+    },
+    colors: ["#f78f1e", "#3359FF", "#7333FF", "#30a6d3", "#775DD0", "#3F51B5", "#4CAF50", "#2B908F", "#2E294E", "#D7263D"],
+    legend: {
+      showForSingleSeries: true,
+      position: "top",
+      labels: {
+        colors: "#565674",
+      },
+    },
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    xaxis: {
+      type: "datetime",
+      min: moment(minDate).startOf("h").valueOf(),
+      max: moment(maxDate).endOf("h").valueOf(),
+      labels: {
+        datetimeUTC: false,
+        format: "MMM dd, HH:mm",
+      },
+      title: {
+        text: "Date Time",
+        style: {
+          color: "#565674",
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      forceNiceScale: true,
+      min: 0,
+      title: {
+        text: yAxisText,
+        style: {
+          color: "#565674",
+          fontSize: "12px",
+        },
+      },
+      labels: {
+        formatter: (val) => {
+          return Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 0,
+          }).format(val);
+        },
+        style: {
+          colors: "#565674",
+          fontSize: "12px",
+        },
+      },
+    },
+    grid: {
+      strokeDashArray: 4,
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    tooltip: {
+      shared: true,
+      style: {
+        fontSize: "12px",
+      },
+      x: {
+        show: true,
+        format: toolTipXFormat,
+      },
+      y: {
+        formatter: (val) => {
+          if (isNaN(val) || val == null) {
+            return "No Data";
+          }
+          return Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(val) + `%`;
+        },
+      },
+    },
+    annotations: {
+      position: "back",
+      yaxis: [
+        {
+          y: 100,
+          strokeDashArray: 0,
+          borderColor: "#007358",
+          label: {
+            borderColor: "#007358",
+            style: {
+              color: "#fff",
+              background: "#007358",
+            },
+            text: "100% Capacity",
+          },
+        },
+      ],
     },
   };
 
@@ -151,9 +267,7 @@ const typicalDay = (height, data, yAxisText, tooltipYMetric) => {
   const perc25 = data.map((a) => a.percentile25);
   const perc10 = data.map((a) => a.percentile10);
   const min = data.map((a) => a.min);
-  const times = data.map(
-    (a) => `${a.time.split(":")[0]}:${a.time.split(":")[1]}`
-  );
+  const times = data.map((a) => `${a.time.split(":")[0]}:${a.time.split(":")[1]}`);
 
   var options = {
     series: [
@@ -203,15 +317,7 @@ const typicalDay = (height, data, yAxisText, tooltipYMetric) => {
       },
       fontFamily: "inherit",
     },
-    colors: [
-      "#ececec",
-      "#bbbbbb ",
-      "#747474",
-      "#000000",
-      "#747474",
-      "#bbbbbb ",
-      "#ececec",
-    ],
+    colors: ["#ececec", "#bbbbbb ", "#747474", "#000000", "#747474", "#bbbbbb ", "#ececec"],
     legend: {
       showForSingleSeries: true,
       position: "top",
@@ -308,11 +414,7 @@ const typicalDay = (height, data, yAxisText, tooltipYMetric) => {
       },
       y: {
         formatter: (val) => {
-          return (
-            Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(
-              val
-            ) + tooltipYMetric
-          );
+          return Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(val) + tooltipYMetric;
         },
       },
     },
